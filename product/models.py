3015724d93b3e8b65cc9b7a1cca5caaa06b2f849
Peter Oh <petersoh414@gmail.com>
2020-12-20 19:42:12 +0900

    모델링 수정, 확인 및 merge 부탁드립니다

--- product/models.py
+++ product/models.py
@@ -20,13 +20,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+class Discount(models.Model):
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        db_table = "discounts"
+
 class Product(models.Model):
-    name           = models.CharField(max_length=50)
-    price          = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at     = models.DateTimeField(auto_now_add=True)
-    updated_at     = models.DateTimeField(auto_now=True)
-    category       = models.ForeignKey(Category, on_delete=models.CASCADE)
-    likes          = models.ManyToManyField(User, through="Wishlist")
+    name        = models.CharField(max_length=50)
+    price       = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    category    = models.ForeignKey(Category, on_delete=models.CASCADE)
+    likes       = models.ManyToManyField(User, through="Wishlist")
+    is_in_stock = models.BooleanField(default=True)
+    discount    = models.ForeignKey(Discount, on_delete=models.CASCADE, null=True)
 
     class Meta:
         db_table = "products"
