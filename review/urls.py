e910456c5a63dd086e7580deac8b65e67d95af61
Peter Oh <petersoh414@gmail.com>
2020-12-24 11:05:07 +0900

    ADD: RESTful API: review_id

--- review/urls.py
+++ review/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from review.views import ReviewView
+from review.views import ReviewView, ReviewIDView
 
 urlpatterns = [
     path('/reviews', ReviewView.as_view()),
-  #  path('/
+    path('/<int:review_id>', ReviewIDView.as_view()),
 ]
